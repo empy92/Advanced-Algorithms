73b880c6dc35de324d4d1719bd82df59a284b2dc
empy92 <empy92@hotmail.it>
2014-03-09 00:14:30 +0100

    add cos(x)

---
+++ points_gen/cos(x).cc
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159265
+#define NPOINTS 1000
+#define MAX_ERROR 0.0
+
+int main ()
+{
+	int i;
+	cout << "x ;"<< " sen(x) " << endl;
+
+	for(i=0; i<NPOINTS; i++){
+		double x = ((double)i/NPOINTS) * 2.0 * PI; //divide 0--2PI in NPOINTS parts
+		cout << x << " ; " << cos(x) + MAX_ERROR*((2.0* rand()/RAND_MAX) -1.0) << endl; //sinX + (random number from +1.0 to -1.0)*MAX_ERROR
+	}
+	return 0;
+}
