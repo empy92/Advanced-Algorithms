d5029973d17e7aa129f6e6cbbb2a5a0501fa4c77
empy92 <empy92@hotmail.it>
2014-03-10 12:42:26 +0100

    add output

--- points_gen/sin(x).cc
+++ points_gen/sin(x).cc
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #define PI 3.14159265
@@ -9,11 +10,14 @@ using namespace std;
 
 int main ()
 {
-	cout << "x ;"<< " sen(x) " << endl;
+	ofstream out;
+	out.open("sinX.cvs");
+	out << "x ;"<< " sen(x) " << endl;
 
 	for(int i=0; i<NPOINTS; i++){
 		double x = ((double)i/NPOINTS) * 2.0 * PI; //divide 0--2PI in NPOINTS parts
-		cout << x << " ; " << sin(x) + MAX_ERROR*((2.0* rand()/RAND_MAX) -1.0) << endl; //sinX + (random number from +1.0 to -1.0)*MAX_ERROR
+		out << x << " ; " << sin(x) + MAX_ERROR*((2.0* rand()/RAND_MAX) -1.0) << endl; //sinX + (random number from +1.0 to -1.0)*MAX_ERROR
 	}
+	out.close();
 	return 0;
 }
